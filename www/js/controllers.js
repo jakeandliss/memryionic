5644c105f856a3a1c8c683c5daa8dfb4b45a0d44
Jake Thompson <jlcompany23@gmail.com>
2015-06-18 22:37:19 -0600

    added scss

---
+++ www/js/controllers.js
@@ -0,0 +1,28 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('EntriesCtrl', function($scope, Entries) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+  
+  $scope.entries = Entries.all();
+  $scope.remove = function(entry) {
+    Entries.remove(entry);
+  }
+})
+
+.controller('EntryDetailCtrl', function($scope, $stateParams, Entries) {
+  $scope.entry = Entries.get($stateParams.EntryId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
---
+++ www/js/services.js
@@ -0,0 +1,50 @@
+angular.module('starter.services', [])
+
+.factory('Entries', function() {
+  // Might use a resource here that returns a JSON array
+
+  // Some fake testing data
+  var entries = [{
+    id: 0,
+    title: 'Ben Sparrow',
+    content: 'You on your way?',
+    image: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+  }, {
+    id: 1,
+    title: 'Max Lynx',
+    content: 'Hey, it\'s me',
+    image: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+  },{
+    id: 2,
+    title: 'Adam Bradleyson',
+    content: 'I should buy a boat',
+    image: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+  }, {
+    id: 3,
+    title: 'Perry Governor',
+    content: 'Look at my mukluks!',
+    image: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+  }, {
+    id: 4,
+    title: 'Mike Harrington',
+    content: 'This is wicked good ice cream.',
+    image: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+  }];
+
+  return {
+    all: function() {
+      return entries;
+    },
+    remove: function(entry) {
+      entries.splice(entries.indexOf(entry), 1);
+    },
+    get: function(EntryId) {
+      for (var i = 0; i < entries.length; i++) {
+        if (entries[i].id === parseInt(entryId)) {
+          return entries[i];
+        }
+      }
+      return null;
+    }
+  };
+});
