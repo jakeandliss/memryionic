14b7a5c228ea527ed44628fd5504ab390d317a1e
Jake Thompson <jlcompany23@gmail.com>
2015-06-29 21:27:59 -0600

    working on entry views for visual, audio, and files

--- www/js/services.js
+++ www/js/services.js
@@ -9,14 +9,18 @@ angular.module('starter.services', [])
     title: 'When I was on my way home today this happened...',
     content: 'You on your way? Because there is a really cool party and I want to go to it really really bad. There is only one way to go. With a friend.',
     resources: [
-    { attachment: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'},
-    { attachment: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'},
+    {
+      attachment: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png',
+      attachment_content_type: "image"
+    },
+    {
+      attachment: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460',
+      attachment_content_type: "image"
+    },
     {
       attachment: 'http://techslides.com/demos/sample-videos/small.mp4',
       attachment_content_type: "video"
-    }
-
-    ],
+    }],
     date: "2012-04-23",
     tags: [
       {name: 'tag'},
