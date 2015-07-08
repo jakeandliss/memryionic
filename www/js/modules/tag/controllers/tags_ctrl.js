5695c58ca35da0351be5385aee9933bf8431737e
Ashish Singh <aitashish173@gmail.com>
2015-07-09 03:49:02 +0530

    removed a log statement

--- www/js/modules/tag/controllers/tags_ctrl.js
+++ www/js/modules/tag/controllers/tags_ctrl.js
@@ -4,7 +4,7 @@
 
     TagsCtrl.$inject = ['$state', '$scope', 'Tags'];
     function TagsCtrl($state, $scope, Tags){
-      console.log('in tags ctrl');
+
       $scope.tags = Tags.all();
       $scope.remove = function(tag) {
         Tags.remove(tag);
