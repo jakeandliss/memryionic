34ba66e430d467e439dce661fdc802effd562944
Ashish Singh <aitashish173@gmail.com>
2015-07-12 00:03:51 +0530

    Moved tags factory to tags module from entry module

--- www/js/modules/tag/services/tag-service.js
+++ www/js/modules/tag/services/tag-service.js
@@ -1,12 +1,54 @@
 (function(){
-  angular.module('memryApp')
-    .service('TagService', TagService);
+  'use strict';
 
-    TagService.$inject = ['$state'];
-    function TagService($state){
-      console.log('In tag service');
-      this.demoMethod = function(){
-        console.log('In demo method');
+  angular.module('memryApp')
+    .factory('Tags', Tags);
+
+    Tags.$inject = [];
+    function Tags(){
+      var tags = [
+        {
+          name: "test 0",
+          id: 0,
+          parent_id: ""
+        },
+        {
+          name: "test 1",
+          id: 1,
+          parent_id: "0"
+        },
+        {
+          name: "test 2",
+          id: 2,
+          parent_id: "0"
+        },
+        {
+          name: "test 3",
+          id: 3,
+          parent_id: "0"
+        },
+        {
+          name: "test 4",
+          id: 4,
+          parent_id: "1"
+        }
+      ];
+
+      return {
+        all: function() {
+          return tags;
+        },
+        remove: function(tag) {
+          tags.splice(entries.indexOf(tag), 1);
+        },
+        get: function(TagId) {
+          for (var i = 0; i < entries.length; i++) {
+            if (tags[i].id === parseInt(tagId)) {
+              return tags[i];
+            }
+          }
+          return null;
+        }
       };
     }
 })();
