5fcfb6d5d9916d7ce2d3a061cb15ce633410e41c
Jake Thompson <jlcompany23@gmail.com>
2015-07-13 13:58:29 -0600

    tags nested under parent tags

--- www/js/modules/tag/services/tag-service.js
+++ www/js/modules/tag/services/tag-service.js
@@ -9,33 +9,53 @@
       var tags = [
         {
           name: "test 0",
-          id: 0,
+          id: "0",
           parent_id: ""
         },
         {
           name: "test 1",
-          id: 1,
+          id: "1",
           parent_id: "0"
         },
         {
           name: "test 2",
-          id: 2,
+          id: "2",
           parent_id: "0"
         },
         {
           name: "test 3",
-          id: 3,
+          id: "3",
           parent_id: "0"
         },
         {
           name: "test 4",
-          id: 4,
+          id: "4",
           parent_id: "1"
         },
         {
           name: "test 5",
-          id: 5,
+          id: "5",
           parent_id: ""
+        },
+        {
+          name: "test 6",
+          id: "6",
+          parent_id: "5"
+        },
+        {
+          name: "test 7",
+          id: "7",
+          parent_id: "5"
+        },
+        {
+          name: "test 8",
+          id: "8",
+          parent_id: "4"
+        },
+        {
+          name: "test 9",
+          id: "9",
+          parent_id: "8"
         }
       ];
 
