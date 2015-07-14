71704927950c143e9664f187068214e918049254
Jake Thompson <jlcompany23@gmail.com>
2015-07-14 17:24:22 -0600

    working on forgot password dialogue box

--- www/js/modules/user/controllers/UserCtrl.js
+++ www/js/modules/user/controllers/UserCtrl.js
@@ -2,12 +2,24 @@
   angular.module('memryApp')
     .controller('UserCtrl', UserCtrl);
 
-    UserCtrl.$inject = ['$state', '$scope'];
-    function UserCtrl($state, $scope){
+    UserCtrl.$inject = ['$state', '$scope', '$mdDialog'];
+    function UserCtrl($state, $scope, $mdDialog){
       $scope.user = {
         first_name: "Jake",
         last_name: "Thompson",
         email: "test@gmail.com"
       };
+
+      $scope.showDialog = function(ev) {
+        $mdDialog.show({
+          templateUrl: 'forgot-password-dialog.html',
+          parent: angular.element(document.body),
+          targetEvent: ev,
+        })
+      };
+
+      $scope.closeDialog = function() {
+          $mdDialog.hide();
+        }
     };
 })();
