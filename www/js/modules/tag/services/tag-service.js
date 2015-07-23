63bf8302eea5e229969fc17aa7d3b1b999e19cc9
Jake Thompson <jlcompany23@gmail.com>
2015-07-23 16:28:28 -0600

    updated tag json structure in services

--- www/js/modules/tag/services/tag-service.js
+++ www/js/modules/tag/services/tag-service.js
@@ -10,52 +10,129 @@
         {
           name: "test 0",
           id: "0",
-          parent_id: ""
+          ancestry: "",
+          children: [
+            {
+              name: "test 0 child",
+              id: "10",
+              ancestry: "0",
+              children: [
+                {
+                  name: "test 0 child child",
+                  id: "18",
+                  ancestry: "10"
+                }
+              ]
+            }
+          ]
         },
         {
           name: "test 1",
           id: "1",
-          parent_id: "0"
+          ancestry: "0",
+          children: [
+            {
+              name: "test 1 child",
+              id: "11",
+              ancestry: "1",
+            }
+          ]
         },
         {
           name: "test 2",
           id: "2",
-          parent_id: "0"
+          ancestry: "0",
+          children: [
+            {
+              name: "test 2 child",
+              id: "12",
+              ancestry: "2",
+            }
+          ]
         },
         {
           name: "test 3",
           id: "3",
-          parent_id: "0"
+          ancestry: "0",
+          children: [
+            {
+              name: "test 3 child",
+              id: "13",
+              ancestry: "3",
+            }
+          ]
         },
         {
           name: "test 4",
           id: "4",
-          parent_id: "1"
+          ancestry: "1",
+          children: [
+            {
+              name: "test 4 child",
+              id: "14",
+              ancestry: "4",
+            }
+          ]
         },
         {
           name: "test 5",
           id: "5",
-          parent_id: ""
+          ancestry: "",
+          children: [
+            {
+              name: "test 5 child",
+              id: "15",
+              ancestry: "5",
+            }
+          ]
         },
         {
           name: "test 6",
           id: "6",
-          parent_id: "5"
+          ancestry: "5",
+          children: [
+            {
+              name: "test 6 child",
+              id: "16",
+              ancestry: "6",
+            }
+          ]
         },
         {
           name: "test 7",
           id: "7",
-          parent_id: "5"
+          ancestry: "5",
+          children: [
+            {
+              name: "test 7 child",
+              id: "17",
+              ancestry: "7",
+            }
+          ]
         },
         {
           name: "test 8",
           id: "8",
-          parent_id: "4"
+          ancestry: "4",
+          children: [
+            {
+              name: "test 8 child",
+              id: "18",
+              ancestry: "8",
+            }
+          ]
         },
         {
           name: "test 9",
           id: "9",
-          parent_id: "8"
+          ancestry: "8",
+          children: [
+            {
+              name: "test 9 child",
+              id: "19",
+              ancestry: "9"
+            }
+          ]
         }
       ];
 
