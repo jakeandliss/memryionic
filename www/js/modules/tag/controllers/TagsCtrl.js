3e716159352c89076748036e4d90391156232730
Anshuman Jasrotia <anshumanjasrotia@gmail.com>
2015-07-28 04:24:22 +0530

    Worked on showing child element on click on the parent element
    Worked on showing child element on click on the parent element
    Displayed the edit form on click of the edit button
    Added edit button along with the delete button in the list swipe

--- www/js/modules/tag/controllers/TagsCtrl.js
+++ www/js/modules/tag/controllers/TagsCtrl.js
@@ -2,16 +2,62 @@
   angular.module('memryApp')
     .controller('TagsCtrl', TagsCtrl);
 
-    TagsCtrl.$inject = ['$state', '$scope', 'Tags'];
-    function TagsCtrl($state, $scope, Tags){
-
+    TagsCtrl.$inject = ['$state', '$stateParams', '$scope', 'Tags'];
+    function TagsCtrl($state, $stateParams, $scope, Tags){
       $scope.tags = Tags.all();
+      $scope.selectedTab = 1;
+      $scope.filteredTags;
+      $scope.label = "Label";
+      $scope.labelId = null;
+      $scope.edit = false;
+
+      $scope.filterTags = function(){
+
+        if($stateParams.id)
+        {  
+          
+          var filterResult = $scope.tags.filter(function(elem){
+              return elem.id == $stateParams.id; 
+            });
+          $scope.filteredTags = filterResult[0].children;
+          $scope.label = filterResult[0].name;
+          $scope.labelId = filterResult[0].id;
+        }
+        else
+        {
+          $scope.filteredTags = $scope.tags;
+        }
+      };
+      $scope.filterTags();
+
+
       $scope.remove = function(tag) {
         Tags.remove(tag);
       };
 
+      $scope.edit = function(tag)
+      {
+          $scope.isEditing = true;
+      };
+
+      $scope.save = function()
+      {
+          
+      };
+
+      $scope.cancel = function()
+      {
+           $scope.isEditing = false;
+      };
+
+      $scope.editLabel =  function()
+      {
+        $scope.isEditing = true;
+      }
+      
       $scope.shouldShowDelete = false;
       $scope.listCanSwipe = true
-
+      
+      
     };
 })();
--- www/js/modules/tag/services/tag-service.js
+++ www/js/modules/tag/services/tag-service.js
@@ -9,40 +9,40 @@
       var tags = [
         {
           name: "test 0",
-          id: "0",
+          id: 0,
           ancestry: "",
           children: [
             {
               name: "test 0 child",
-              id: "10",
+              id: 10, 
               ancestry: "0",
               children: [
                 {
-                  name: "test 0 child child",
-                  id: "18",
+                  name: "test 0 child child", 
+                  id: 18,
                   ancestry: "10",
                   children: [
                     {
                       name: "test 0 child child child",
-                      id: "19",
+                      id: 19,
                       ancestry: "18"
                     },
                     {
                       name: "test 0 child child child",
-                      id: "20",
+                      id: 20,  
                       ancestry: "18"
                     },
                     {
                       name: "test 0 child child child",
-                      id: "21",
+                      id: 21,
                       ancestry: "18"
                     },
                     {
                       name: "test 0 child child child",
-                      id: "22",
+                      id: 22,
                       ancestry: "18"
                     }
-                  ]
+                  ]  
                 }
               ]
             }
@@ -50,111 +50,116 @@
         },
         {
           name: "test 1",
-          id: "1",
-          ancestry: "",
+          id: 1,
+          ancestry: "",  
           children: [
             {
               name: "test 1 child",
-              id: "11",
+              id: 11,   
               ancestry: "1",
             }
           ]
         },
         {
           name: "test 2",
-          id: "2",
+          id: 2,
           ancestry: "",
           children: [
-            {
+            {   
               name: "test 2 child",
-              id: "12",
-              ancestry: "2",
+              id: 12,
+              ancestry: "2",     
             }
-          ]
+          ]  
         },
         {
           name: "test 3",
-          id: "3",
+          id: 3,
           ancestry: "",
           children: [
             {
               name: "test 3 child",
-              id: "13",
+              id: 13,
               ancestry: "3",
             }
           ]
         },
         {
           name: "test 4",
-          id: "4",
+          id: 4,
           ancestry: "",
           children: [
             {
               name: "test 4 child",
-              id: "14",
+              id: 14,
               ancestry: "4",
             }
           ]
         },
         {
           name: "test 5",
-          id: "5",
+          id: 5,
           ancestry: "",
-          children: [
+          children: [ 
             {
               name: "test 5 child",
-              id: "15",
+              id: 15,
               ancestry: "5",
             }
           ]
         },
         {
           name: "test 6",
-          id: "6",
+          id: 6,
           ancestry: "",
           children: [
             {
               name: "test 6 child",
-              id: "16",
+              id: 16,
               ancestry: "6",
             }
           ]
         },
         {
           name: "test 7",
-          id: "7",
+          id: 7,
           ancestry: "",
           children: [
             {
               name: "test 7 child",
-              id: "17",
+              id: 17,
               ancestry: "7",
             }
           ]
         },
-        {
+        {         
           name: "test 8",
-          id: "8",
+          id: 8,
           ancestry: "",
           children: [
             {
               name: "test 8 child",
-              id: "18",
+              id: 18,
               ancestry: "8",
             }
           ]
         },
         {
           name: "test 9",
-          id: "9",
+          id: 9,  
           ancestry: "",
-          children: [
+          children: [     
             {
-              name: "test 9 child",
-              id: "19",
+              name: "test 9 child",    
+              id: 19,
               ancestry: "9"
-            }
-          ]
+            },
+            {
+              name: "test 9 child2",
+              id: 99,
+              ancestry: "9"
+            },
+          ] 
         }
       ];
 
