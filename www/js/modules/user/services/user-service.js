e3866965b2dcffa30f4ae8186369eb6765c2b881
Expert DotNet <anshumanjasrotia@gmail.com>
2015-10-21 05:16:56 +0530

    Worked on Registration

--- www/js/modules/user/services/user-service.js
+++ www/js/modules/user/services/user-service.js
@@ -2,11 +2,37 @@
   angular.module('memryApp')
     .service('UserService', UserService);
 
-    UserService.$inject = ['$state'];
-    function UserService($state){
-      console.log('In user service');
-      this.demoMethod = function(){
-        console.log('In demo method of demo user service');
-      };
+    UserService.$inject = ['$state','$http'];
+    function UserService($state,$http){
+      var Users=[{
+      		id: 1,
+      		email:"test@gmail.com",
+      		firstName:"test",
+      		lastName:"user1"
+      	},{
+      		id: 2,
+      		email:"test1@gmail.com",
+      		firstName:"test1",
+      		lastName:"user2"
+      	},{
+      		id: 3,
+      		email:"test2@gmail.com",
+      		firstName:"test2",
+      		lastName:"user3"
+      	}
+      ]
+      var baseAddress="http://104.236.57.97:8085/";
+      return {
+      	getUser:function(email){
+      		var filterUser=Users.filter(function(elem){
+      			return elem.email==email;
+      		});
+      		return filterUser
+      	},
+        createAccount:function(user){
+          var _userRegistration={"userRegistration":user};
+          return $http({method:"POST",url:baseAddress+"api/v1/users.json",data:_userRegistration})
+        }
+      }
     }
 })();
