4cdaa88aae4dde7212cd422d2a1069d12e6ceb15
Expert DotNet <anshumanjasrotia@gmail.com>
2015-10-31 04:13:29 +0530

    Worked on fixing the panels

---
+++ www/js/user/controllers/UserCtrl.js
@@ -0,0 +1,45 @@
+(function() {
+  angular.module('memryApp')
+    .controller('UserCtrl', UserCtrl);
+
+    UserCtrl.$inject = ['$state', '$scope', '$mdDialog','UserService'];
+    function UserCtrl($state, $scope, $mdDialog,UserService){
+      $scope.user = {
+        first_name: "Jake",
+        last_name: "Thompson",
+        email: "test@gmail.com",
+        avatar: "https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png"
+      };
+      $scope.newUser={}
+      $scope.isSubmit=false;
+      $scope.messages=[];
+      $scope.showErrorMessage=false;
+      $scope.showSuccessMessage=false;
+      $scope.showDialog = function(ev) {
+        $mdDialog.show({
+          templateUrl: 'forgot-password-dialog.html',
+          parent: angular.element(document.body),
+          targetEvent: ev,
+          controller: 'UserCtrl'
+        })
+      };
+
+      $scope.closeDialog = function() {
+          $mdDialog.hide();
+      }
+      $scope.createAccount=function(){
+        $scope.messages=[];
+        $scope.showErrorMessage=false;
+        $scope.showSuccessMessage=false;
+          UserService.createAccount($scope.newUser).success(function(responce){
+            $scope.showSuccessMessage=true;
+            $scope.newUser={}
+            console.log(responce)
+          }).error(function(responce){
+            $scope.showErrorMessage=true;
+            console.log(responce.message.errors)
+            $scope.messages=responce.message.errors;
+          })
+      }
+    };
+})();
---
+++ www/js/user/module.js
@@ -0,0 +1,89 @@
+// User module config file
+
+(function(){
+  angular.module('memryApp')
+    .config(Config);
+
+    Config.$inject = ['$stateProvider'];
+    function Config($stateProvider){
+      $stateProvider
+        .state('user', {
+          url: '/user',
+          abstract: true,
+          templateUrl: function(){
+            return 'js/modules/user/views/' + window.templateMode + '/user.html';
+          },
+          controller: 'UserCtrl'
+        })
+        // user new state
+        .state('user.new', {
+          url: '/new',
+          views: {
+            'user-new': {
+              templateUrl: function(){
+                return 'js/modules/user/views/' + window.templateMode + '/new.html';
+              },
+              controller: 'UserCtrl'
+            }
+          }
+        })
+
+        .state('user.login', {
+          url: '/login',
+          views: {
+            'user-login': {
+              templateUrl: function() {
+                return 'js/modules/user/views/' + window.templateMode + '/login.html';
+              },
+              controller: 'UserCtrl'
+            }
+          }
+        })
+
+        .state('user.forgot-password', {
+          url: '/forgot-password',
+          views: {
+            'forgot-password': {
+              templateUrl: function(){
+                return 'js/modules/user/views/' + window.templateMode + '/new_password.html';
+              },
+              controller: 'UserCtrl'
+            }
+          }
+        })
+
+        // user profile
+        .state('app.profile', {
+          url: '/profile',
+          views: {
+            'profile': {
+              templateUrl: function(){
+                return 'js/modules/user/views/' + window.templateMode + '/profile.html';
+              },
+              controller: 'UserCtrl'
+            }
+          }
+        })
+        // edit a user profile
+        .state('userEdit', {
+          url: '/user/edit',
+
+          templateUrl: function(){
+            return 'js/modules/user/views/' + window.templateMode + '/edit.html';
+          },
+          controller: 'UserCtrl'
+        })
+        // update password
+        .state('user.update-password', {
+          url: '/update-password',
+          views: {
+            'update-password': {
+              templateUrl: function(){
+                return 'js/modules/user/views/' + window.templateMode + '/update_password.html';
+              },
+              controller: 'UserCtrl'
+            }
+          }
+        })
+    };
+})();
